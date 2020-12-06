7a45d6209be0fb9c3a354f73163ec891ed004aab
Brendon Pagano <brendonpagano@gmail.com>
2020-12-06 04:05:38 -0300

    Application executioner

---
+++ app.js
@@ -0,0 +1,16 @@
+// Node Modules
+require('dotenv/config');
+const cron = require('node-cron');
+
+// Application Modules
+const server = require('./src/server');
+const Crawler = require('./src/crawler');
+
+// Run every 10 minutes.
+cron.schedule('* * * * *', async () => {
+  console.log('Attempting to crawl page');
+  await Crawler.crawl();
+  await Crawler.saveAsJSON();
+});
+
+server.initializeServer();
