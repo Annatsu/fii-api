f84041c49ad009ab6614a93393fc61407239e4e0
Brendon Pagano <brendonpagano@gmail.com>
2020-12-09 15:30:05 -0300

    Run cron job every 10 minutes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = require('./src/server');
 const Crawler = require('./src/crawler');
 
 // Run every 10 minutes.
-cron.schedule('* * * * *', async () => {
+cron.schedule('*/10 * * * *', async () => {
   console.log('Attempting to crawl page');
   await Crawler.crawl();
   await Crawler.saveAsJSON();
